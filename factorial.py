0c4ea4135be2530eb53458b6561ff583bc3eb466
venkataraman2005 <144318388+venkataraman2005@users.noreply.github.com>
2023-09-07 12:47:57 +0530

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-#finding factorial
-def fact(n):
-  if(n==0 or n==1):
-      return 1
-  else:
-     return n*fact(n-1)
-  number=5
-  result=fact(number)
-  printf("the factorial of {}is {}".format(number.result))
